03983d77cdc6e492a560d9373031129c47b37630
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-15 11:47:52 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % 1 == 0 && n != i)
+		{
+			n /= i;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}

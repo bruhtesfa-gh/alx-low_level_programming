6d158eb74d537df9963f2b761bf193240280068c
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-15 08:11:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints statement
+ *
+ * Description: a C program that prints exactly with proper grammar, but the
+ * outcome is a piece of art, followed by a new line.
+ *
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

3107e4135c64804f7daf933a8dcc63290ad4ff5e
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-15 11:48:13 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - output the std
+ *
+ * Return: print out numbers
+ */
+
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	for (; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

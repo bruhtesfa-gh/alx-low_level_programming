cc476ce35488c97de839e87d8155db6fcee33c46
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-15 08:11:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints statement
+ *
+ * Description: This programme prints exactly Programming is like
+ * building a multilingual puzzle, followed by a new line.
+ *
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

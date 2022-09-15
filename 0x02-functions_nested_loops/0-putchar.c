87a2916379ca3823c52c6e8780764b0e1f6b12ed
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-15 08:19:32 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar and a new line
+ *
+ * Description: This programme prints out _putchar
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}

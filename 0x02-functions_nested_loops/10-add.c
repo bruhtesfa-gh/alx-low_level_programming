200ff59a0ae5ee5cca8f7c2d31f3d17cd3feabaf
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-15 08:19:39 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point for programme
+ *
+ * @a: first number to add
+ *
+ * @b: second number to add
+ *
+ * Description: Function adds two numbers
+ *
+ * Return: Return the sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

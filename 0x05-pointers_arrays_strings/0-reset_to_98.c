6ef8efd673395483bb20c2303aa600cfae62f8e2
Bruk <bruhtesfazelealem436@gmail.com>
2022-09-23 12:04:53 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function takes a pointer to an int and updates the value
+ *
+ * @n: pointer to an integer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
